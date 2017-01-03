d2f81f5a46700011937960ded0a605792cada9f7
Max <raizzz013@gmail.com>
2017-01-03 15:56:19 -0800

    Exp_07, some food for brain) MadMax

---
+++ src/test/java/test/Exp_07_IHaveNoIdeaHowItWork.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by MadMax on 1/3/2017.
+ */
+public class Exp_07_IHaveNoIdeaHowItWork {
+
+    @Test
+    public int test(int n, int k)
+    {
+        // Basically it is delete k-th bit from n-th number (In binary format) TODO Understand how it work !!!
+        return n & ~(1 << (k - 1));
+    }
+}
